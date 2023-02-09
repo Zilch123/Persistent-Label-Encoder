a18139bd95d4f97754046cc373d383534b95c218
Zilch123 <timontunes@gmail.com>
2023-02-09 16:40:16 +0530

    setup.py variable name changed

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author_email="<timontunes@gmail.com>",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     packages=find_packages(),
     install_requires=['sklearn'],
     keywords=['Inference', 'Label encoder', 'Persistent Label Encoder'],
